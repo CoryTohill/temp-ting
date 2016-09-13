977d2b4c60d0efcc36973bb38ae10cd2fb622353
Cory Tohill <corytohill@gmail.com>
2016-09-13 15:48:35 -0500

    prevented threads from starting if already loggin temps

--- temptingApp/urls.py
+++ temptingApp/urls.py
@@ -16,4 +16,3 @@ urlpatterns = [
     url(r'^start_logging_temps', csrf_exempt(views.start_logging_temps)),
     url(r'^stop_logging_temps', csrf_exempt(views.stop_logging_temps)),
 ]
-# csrf_exempt(views.LoginView.as_view())
--- temptingApp/views.py
+++ temptingApp/views.py
@@ -1,16 +1,18 @@
-from .models import Team, TempLog, Temp
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from rest_framework import viewsets
+from .models import Team, TempLog, Temp
 from .serializers import TeamSerializer, TempLogSerializer, TempSerializer, UserSerializer
-import threading
-import os,sys
 from yocto_api import *
 from yocto_temperature import *
-import json
-from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from . import models
+import threading, json
 
+
+# global threading event
 e = threading.Event()
+e.clear()
+
 
 class Team(viewsets.ModelViewSet):
     queryset = Team.objects.all()
@@ -20,6 +22,7 @@ class Team(viewsets.ModelViewSet):
 class TempLog(viewsets.ModelViewSet):
     queryset = TempLog.objects.all()
     serializer_class = TempLogSerializer
+
 
 class Temp(viewsets.ModelViewSet):
     queryset = Temp.objects.all()
@@ -34,81 +37,54 @@ class User(viewsets.ModelViewSet):
 def start_logging_temps(request):
     data = json.loads(request.body.decode("utf-8"))
 
+    thermometer = data["thermometer"]
     temp_log_id = data["temp_log_id"]
 
     temp_log = models.TempLog.objects.filter(id=temp_log_id)[0]
 
-    errmsg=YRefParam()
-
-
-    target = "THRMCPL1-6F5AC"
+    # Yocto api error message
+    errmsg = YRefParam()
 
     # Setup the API to use local USB devices
     if YAPI.RegisterHub("usb", errmsg)!= YAPI.SUCCESS:
         return HttpResponse(status=400)
 
-    # retreive any temperature sensor
+    # retreive any temperature sensor and check that it is online
     sensor = YTemperature.FirstTemperature()
 
     if sensor is None :
         return HttpResponse(status=400)
-
     else:
-        sensor = YTemperature.FindTemperature(target + '.temperature1')
+        sensor = YTemperature.FindTemperature(thermometer + '.temperature1')
+    if not(sensor.isOnline()):
+        return HttpResponse(status=400)
 
-    if not(sensor.isOnline()):return HttpResponse(status=400)
-
-    # retreive module serial
+    # retreive sensor serial
     serial = sensor.get_module().get_serialNumber()
 
-
-    # retreive channel
+    # retreive thermometer channel
     channel1 = YTemperature.FindTemperature(serial + '.temperature1')
     print(channel1.get_currentValue())
 
+    # if the current thermometer is already logging data, send a 400 status
+    if thermometer in [thread.name for thread in threading.enumerate()]:
+        return HttpResponse(status=400)
 
+    # target method to start a logging thread
     def start_log(e):
+        e.clear()
         while not e.isSet():
             current_temp = round(channel1.get_currentValue())
             models.Temp.objects.create(value=current_temp, temp_log=temp_log)
             print(current_temp)
             e.wait(2)
-            # YAPI.Sleep(1000)
 
-    thread = threading.Thread(target=start_log, args=(e,))
-    # thread = threading.Thread(target=start_log)
-
+    thread = threading.Thread(name=thermometer, target=start_log, args=(e,))
     thread.start()
+
     return HttpResponse(status=200)
 
 
 def stop_logging_temps(request):
-
     e.set()
     return HttpResponse(status=200)
-
-
-
-# def create_event(request):
-#     data = json.loads(request.body.decode("utf-8"))
-
-#     name = data["name"]
-#     description = data["description"]
-#     start_time = data["start_time"]
-#     end_time = data["end_time"]
-#     max_tickets = data["max_tickets"]
-#     venue = Venue.objects.get(pk=data["venue"])
-
-#     try:
-#         event_obj = Event(name=name,
-#                           description=description,
-#                           start_time=start_time,
-#                           end_time=end_time,
-#                           max_tickets=max_tickets,
-#                           venue=venue)
-#         event_obj.save()
-
-#         return HttpResponse(status=200)
-
-#     except IntegrityError:
-#         return HttpResponse(status=400)
