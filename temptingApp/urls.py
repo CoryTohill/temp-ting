643b4aea2cc076cdc752167d800e22bc0e757c8c
Cory Tohill <corytohill@gmail.com>
2016-09-13 16:09:29 -0500

    created login view and url

--- temptingApp/urls.py
+++ temptingApp/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^start_logging_temps', csrf_exempt(views.start_logging_temps)),
     url(r'^stop_logging_temps', csrf_exempt(views.stop_logging_temps)),
+    url(r'^login', csrf_exempt(views.user_login)),
 ]
